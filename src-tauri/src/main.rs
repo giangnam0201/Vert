2494e36721033f3a02e29852f0acbb7f5b56acdd
Namdev <admin@namdev.qzz.io>
2026-04-30 13:04:42 +0700

    feat: implement cross-platform CI/CD pipeline and enforce single-window constraint in main application logic

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,10 +5,6 @@
 
 fn main() {
   tauri::Builder::default()
-    .on_navigation(|url| {
-        let url_str = url.as_str();
-        url_str.starts_with("tauri://") || url_str.starts_with("https://tauri.localhost") || url_str.starts_with("http://tauri.localhost")
-    })
     .on_window_event(|event| {
         let window = event.window();
         if window.label() != "main" {
